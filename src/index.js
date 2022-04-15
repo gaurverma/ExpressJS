61dc482d347f60ab53f17158f63375be8bf6d6f0
gaurverma <gaurverma333@gmail.com>
2022-04-15 18:21:30 +0530

    Handled erorr page

--- src/index.js
+++ src/index.js
@@ -14,39 +14,19 @@ app.set("views",tempPath);
 
 app.get("/",(req,res)=>{
     res.render("index");
-    //res.render('index.hbs');
 })
 
 app.get("/about",(req,res)=>{
-    
-})
-
-// to show the pages not found in the about page
-
-app.get("/about/*",(req,res)=>{
-   
-})
-
-//to take the parameters in query String and display them
-
-app.get('/temp',(req,res)=>{
-   
+    res.render("about");
 })
 
 app.get('/weather',(req,res)=>{
     res.render("weather");
 })
 
-// to show the pages not found
-
 app.get('*',(req,res)=>{
-    
+    res.render("404");
 })
-
-app.get("/",(req,res)=>{
-    res.send("Hello from the home page");
-});
-
 
 app.listen(8000,()=>{
     console.log("server is listening");
